f6c43ac966ee87ad2cd700d371927ce14a2ce9b7
JCUI21 <jcui21@ford.com>
2023-05-06 17:48:21 +0800

    ui

---
+++ src/main/resources/vue-demo/src/views/Login/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="login-body">
+    <canvas id="login-canvas" style="width: 100%;height: 100%"/>
+  </div>
+</template>
+
+<script>
+import "@/assets/js/canvas"
+import { onMounted } from 'vue';
+export default {
+  name: 'Login',
+  components: {
+  },
+  props: {
+  },
+  data() {
+    return {
+    }
+  },
+  setup() {
+    onMounted(() =>  {
+      debugger
+    })
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.login-body {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
